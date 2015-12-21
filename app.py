0b7ba30c9b65d330db8541b438b048374b94aa11
David Song <songdavid98@gmail.com>
2015-12-21 11:16:59 -0500

    updated app

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template,request
+import utils, urllib2, json, random
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
 
 if __name__ == "__main__":
    app.debug = True
