8afe4dc7740484144ae4711cac06f67c70a55769
tonyli7 <xxtonyxxlixx@gmail.com>
2015-12-22 11:14:35 -0500

    api keys

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import json
-#key: ab64222e93da26b4aba758d80daa792810d18ecd
-def search(name):
+#tmdb api key https://api.themoviedb.org/3/movie/550?api_key=###
+#0adca0f6cd83c5390b72d746f4df63e7
+
+#key: ab64222e93da26b4aba758d80daa792810d18ecd for idreambook
+#link: http://idreambooks.com/api/books/reviews.json?q={keywords}&key={yourAPIkey}
+def search(name): 
     print "hi"
