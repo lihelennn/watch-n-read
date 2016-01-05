84b64970cb244e1d2b6fd4371f183d3bd52a3ae0
David Song <songdavid98@gmail.com>
2016-01-05 11:48:01 -0500

    added some doc

--- utils.py
+++ utils.py
@@ -11,12 +11,24 @@ def spaceConverter(query):
     return newstring
 
 def reviewEvaluation(text):
+    """
+    Will analyze the number of good/bad words and return a ratio good:bad
+    Params:
+        text - (String) Text coming from the content of the review
+    Returns:
+        ratio - (Float) A ratio of good to bad words
+    """
+
     ratio = 0
     exp = "[a-zA-Z]+"
     results = re.finall(exp, text)
     numofwords = len(results)
+
+    exp = "([Yy]es)|([Gg]ood)|([Aa]wesome)|([Gg]reat)|([Bb]est)|([Aa]mazing)"
+    goodresults = re.findall(exp, text)
     
-    exp = "([Nn]ot)|([Tt]oo)|([Dd]idn't)|([Dd]oesn't)|([B]bad)|([Tt]errible)"
-    results = re.findall(exp, text)
-    ratio = 1 - len(results)/numofwords
+    exp = "([Nn]o)|([Nn]ot)|([Tt]oo)|([Dd]idn't)|([Dd]oesn't)|([B]bad)|([Tt]errible)"
+    badresults = re.findall(exp, text)
+    ratio = len(goodresults)/len(badresults)
     return ratio
+
