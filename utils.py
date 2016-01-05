74ccab6a9e1fef5c77786b893e289acbac064baf
David Song <songdavid98@gmail.com>
2016-01-05 11:39:59 -0500

    some regex

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-
+import re, regex
 
 def spaceConverter(query):
     newstring = ""
@@ -9,3 +9,14 @@ def spaceConverter(query):
             newstring += letter
     print newstring
     return newstring
+
+def reviewEvaluation(text):
+    ratio = 0
+    exp = "[a-zA-Z]+"
+    results = re.finall(exp, text)
+    numofwords = len(results)
+    
+    exp = "([Nn]ot)|([Tt]oo)|([Dd]idn't)|([Dd]oesn't)|([B]bad)|([Tt]errible)"
+    results = re.findall(exp, text)
+    ratio = 1 - len(results)/numofwords
+    return ratio
