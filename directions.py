8ba7e680cf285d5b98681e9ecc52ed488b6a96a6
tonyli7 <xxtonyxxlixx@gmail.com>
2016-01-05 11:47:41 -0500

    directions api key

---
+++ directions.py
@@ -0,0 +1,3 @@
+#google directions api key: AIzaSyABf0qNbgpn4l2sUbqTPq1sofwLsQ1ZG-A
+#link: https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY
+
