7a96954767fc24d06c829c355ba0cbacd431ca10
tonyli7 <xxtonyxxlixx@gmail.com>
2016-01-07 11:47:40 -0500

    something?

--- directions.py
+++ directions.py
@@ -1,3 +1,12 @@
-#google directions api key: AIzaSyABf0qNbgpn4l2sUbqTPq1sofwLsQ1ZG-A
+import json, urllib2, utils
+#google embed maps api key: AIzaSyC3JS6akFEzmQqhsa_ny3OoqEt3gDOAWow
 #link: https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY
 
+def getMapLink(origin, dest):
+    url="""https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=AIzaSyC3JS6akFEzmQqhsa_ny3OoqEt3gDOAWow
+"""
+    fixedOrigin=utils.spaceConverter(origin)
+    fixedDest=utils.spaceConverter(dest)
+    url=url%(fixedOrigin)
+    url=url%(fixedDest)
+    return url
