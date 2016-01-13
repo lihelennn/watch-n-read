088e88c18c79766f3277d62a989e3028102633a0
tonyli7 <xxtonyxxlixx@gmail.com>
2016-01-13 11:47:44 -0500

    accounts

--- accounts.py
+++ accounts.py
@@ -8,6 +8,7 @@ def isValid(username, password):
     cursor=db.users.find({'username':username, 'password': password})
     userlist=[]
     for doc in cursor:
+        print doc
         for i in doc:
             userlist+=[str(i)]
 
@@ -31,15 +32,42 @@ def newUser(username,password):
     db.users.insert_one({"username":username,"password":password})
     return True
 
+def newPost(username, title, post):
+    connection = MongoClient()
+    db=connection.wnr
+
+    username=username.lower()
+    db.posts.insert_one({"username": username, "title":title, "content":post})
+    
 def getPostsFromUser(username):
     connection = MongoClient()
     db = connection.wnr
     
     username = username.lower()
-    cursor=db.posts.find("username": username)
+    cursor=db.posts.find({"username": username})
     postlist = []
     for posts in cursor:
-        postlist += [ [ str(posts['username']), str(posts['title']), str(posts['content']) ] ]
+        postlist += [ [ str(post['username']), str(post['title']), str(post['content']) ] ]
     return postlist
-newUser("Tony","wasd")
 
+def getAllPosts():
+    connection = MongoClient()
+    db = connection.wnr
+
+    cursor=db.posts.find()
+    postlist=[]
+    for post in cursor:
+        print post
+        postlist+=[ [ str(post['username']), str(post['title']), str(post['content']) ] ]
+    return postlist
+
+
+newPost("Tony","God","Hi my name is tony li")
+newPost("Tony","What","this is bs....")
+newPost("Tony","I am god","whyyyyyyyyy")
+newPost("Mario","Luigi sucks","it's a me, mario")
+newPost("Luigi","Mario sucks","waaaaa luigi")
+newPost("Wario","war","whaaaaaaam")
+newPost("Wario","io","whaaam")
+
+print getAllPosts()
